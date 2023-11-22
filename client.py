b36959d30579b1f79e795ffe9faf09cd6cd752d3
kimhyunji24 <hyunji4560@gmail.com>
2023-11-22 13:56:20 +0900

    Changes

--- client.py
+++ client.py
@@ -1,29 +1,58 @@
-#CLIENT #
+from socket import *
 
-import socket
-from _thread import *
+# play_game 함수 정의
+def play_game():
+    print("게임을 선택하세요:\n1. 숫자야구\n2. 틱택토\n3. 업다운")
+    choice = input("게임 번호를 입력하세요: ") # 입력을 정수로 변환
+    return choice
+# Client setup
+from socket import socket, AF_INET, SOCK_STREAM
 
-HOST = "127.0.0.1"
-#server에 출력되는 ip를 입력해주세요#
-PORT = 9999
+HOST = 'localhost'
+port = 43875
 
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.connect((HOST, PORT))
+clientSock = socket(AF_INET, SOCK_STREAM)
+clientSock.connect((HOST, port))
 
-def recv_data(client_socket):
-    while True:
-        data = client_socket.recv(1024)
-        print("recive : ", repr(data.decode()))
+print('접속 완료')
 
-start_new_thread(recv_data, (client_socket,))
-print('>> Connect Server')
+# 헤더 읽기
+headerSize = int.from_bytes(clientSock.recv(4), byteorder='big')
+header = clientSock.recv(headerSize)
+print('헤더:', headerSize)
+
+# 바디 읽기
+bodySize = int.from_bytes(clientSock.recv(4), byteorder='big')
+body = clientSock.recv(bodySize)
+print('바디:', bodySize)
+
+
+# 게임 선택을 받아서 서버에 전송
+game_choice = play_game()
+clientSock.send(str(game_choice)).encode()
+
 
 while True:
-    message = input()
-    if message == 'quit':
-        close_data = message
+    message = clientSock.recv(1024).decode()
+    print(message)
+
+    if message == "종료":
         break
 
-    client_socket.send(message.encode())
+    if "축하합니다" in message or "이겼습니다" in message or "무승부" in message:
+        break
 
-client_socket.close()
+    guess = None
+
+    # 게임 선택에 따라 입력 방식을 조정하여 서버에 전송
+    if game_choice == 1:
+        guess = input("숫자야구 게임입니다. 세 자리 수를 맞춰보세요 (0~9 사이의 숫자 중복 없이)")
+    elif game_choice == 2:
+        guess = int(input("0~8 중 빈 곳에 숫자를 입력하세요: "))  # 1~9 대신 0~8로 수정
+    elif game_choice == 3:
+        guess = int(input("1부터 100까지의 숫자를 입력하세요: "))
+
+
+    clientSock.send(str(guess).encode())
+
+clientSock.close()
--- server.py
+++ server.py
@@ -1,71 +1,157 @@
-# SERVER #
+from socket import *
+import threading
+import random
 
-import socket
-from _thread import *
+players = []
 
-client_sockets = []
-# Server IP and Port #
+def number_baseball(player_socket):
+    answer = str(random.randint(100, 999))
+    attempts = 0
 
+    player_socket.send("숫자야구 게임입니다. 세 자리 수를 맞춰보세요 (0~9 사이의 숫자 중복 없이): ".encode())
 
-HOST = socket.gethostbyname(socket.gethostname())
-PORT = 9999
-
-
-# processing in thread #
-# new client, new thread #
-
-def threaded(client_socket, addr):
-    print('>> Connected by :', addr[0], ':', addr[1])
-
-    # process until client disconnect #
     while True:
-        try:
-            # send client if data recieved(echo) #
-            data = client_socket.recv(1024)
+        guess = player_socket.recv(1024).decode()
+        attempts += 1
 
-            if not data:
-                print('>> Disconnected by ' + addr[0], ':', addr[1])
+        if len(guess) != 3 or not guess.isdigit() or len(set(guess)) != 3:
+            player_socket.send("세 자리 수를 정확히 입력하세요 (0~9 사이의 숫자 중복 없이).".encode())
+            continue
+
+        strikes = sum(a == b for a, b in zip(answer, guess))
+        balls = sum(a in answer for a in guess) - strikes
+
+        if strikes == 3:
+            player_socket.send(f"축하합니다! {attempts}번만에 숫자를 맞추셨습니다.".encode())
+            break
+        else:
+            player_socket.send(f"{strikes} 스트라이크, {balls} 볼입니다. 다시 시도하세요.".encode())
+    player_socket.close()
+
+
+def tic_tac_toe(player_socket):
+    board = [" " for _ in range(9)]
+    current_player = "X"
+
+    def print_board():
+        board_str = "\n---------\n".join([" | ".join(board[i:i + 3]) for i in range(0, 9, 3)])
+        player_socket.send(board_str.encode())
+
+    def check_winner():
+        for row in range(0, 9, 3):
+            if board[row] == board[row + 1] == board[row + 2] != " ":
+                return True
+
+        for col in range(3):
+            if board[col] == board[col + 3] == board[col + 6] != " ":
+                return True
+
+        if board[0] == board[4] == board[8] != " " or board[2] == board[4] == board[6] != " ":
+            return True
+
+        return False
+
+    def is_board_full():
+        return " " not in board
+
+    player_socket.send("틱택토 게임입니다. 준비중입니다.".encode())
+    print_board()
+
+    while True:
+        move = int(player_socket.recv(1024).decode())
+
+        if board[move] == " ":
+            board[move] = current_player
+            print_board()
+
+            if check_winner():
+                player_socket.send(f"{current_player} 플레이어가 이겼습니다!".encode())
+                break
+            elif is_board_full():
+                player_socket.send("무승부입니다!".encode())
                 break
 
-            print('>> Received from ' + addr[0], ':', addr[1], data.decode())
-
-            # chat to client connecting client #
-            # chat to client connecting client except person sending message #
-            for client in client_sockets:
-                if client != client_socket:
-                    client.send(data)
-
-        except ConnectionResetError as e:
-            print('>> Disconnected by ' + addr[0], ':', addr[1])
-            break
-
-    if client_socket in client_sockets:
-        client_sockets.remove(client_socket)
-        print('remove client list : ', len(client_sockets))
-
-    client_socket.close()
+            current_player = "O" if current_player == "X" else "X"
+        else:
+            player_socket.send("이미 선택된 위치입니다. 다시 선택하세요.".encode())
+    player_socket.close()
 
 
-# Create Socket and Bind #
 
-print('>> Server Start with ip :', HOST)
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-server_socket.bind((HOST, PORT))
-server_socket.listen()
+def updown_game(player_socket):
+    number_to_guess = random.randint(1, 100)
+    attempts = 0
 
-#cl Client Socket Accept
+    player_socket.send("업다운 게임입니다. 1부터 100까지의 숫자 중 하나를 맞춰보세요.".encode())
 
-try:
     while True:
-        print('>> Wait')
+        guess = player_socket.recv(1024).decode()
+        attempts += 1
 
-        client_socket, addr = server_socket.accept()
-        client_sockets.append(client_socket)
-        start_new_thread(threaded, (client_socket, addr))
-        print("참가자 수 : ", len(client_sockets))
-except Exception as e:
-    print('에러 : ', e)
+        try:
+            guess = int(guess)
+        except ValueError:
+            player_socket.send("숫자를 입력하세요.".encode())
+            continue
 
-finally:
-    server_socket.close()
+        if guess < number_to_guess:
+            player_socket.send("숫자가 너무 작습니다. 다시 시도하세요.".encode())
+        elif guess > number_to_guess:
+            player_socket.send("숫자가 너무 큽니다. 다시 시도하세요.".encode())
+        else:
+            player_socket.send(f"축하합니다! {attempts}번만에 숫자를 맞추셨습니다.".encode())
+            break
+    player_socket.close()
+
+
+
+
+
+def handle_player(player_socket, player_number):
+    # 게임 선택을 받아서 처리
+    player_socket.send("게임을 선택하세요:\n1. 숫자야구\n2. 틱택토\n3. 업다운".encode())
+    game_choice = player_socket.recv(1024).decode()
+
+    if game_choice == '1':
+        number_baseball(player_socket)
+    elif game_choice == '2':
+        tic_tac_toe(player_socket)
+    elif game_choice == '3':
+        updown_game(player_socket)
+    else:
+        player_socket.send("올바른 게임을 선택하세요.".encode())
+
+    player_socket.send("종료".encode())
+    player_socket.close()
+
+# Server setup
+serverSock = socket(AF_INET, SOCK_STREAM)
+HOST = '127.0.0.1'
+port = 43875
+serverSock.bind((HOST, port))
+serverSock.listen(2)
+
+print('%d번 포트로 접속 대기중…' % port)
+
+while True:
+    connectionSock, addr = serverSock.accept()
+    print(str(addr), '에서 접속되었습니다.')
+
+    # 헤더 작성
+    header = b'HEADER'
+    headerSize = len(header).to_bytes(4, byteorder='big')
+    connectionSock.sendall(headerSize + header)
+
+    # 바디 작성
+    body = b'BODY'
+    bodySize = len(body).to_bytes(4, byteorder='big')
+    connectionSock.sendall(bodySize + body)
+
+    players.append(connectionSock)
+
+    if len(players) == 2:
+        break
+
+for i, player_socket in enumerate(players):
+    player_thread = threading.Thread(target=handle_player, args=(player_socket, i + 1))
+    player_thread.start()
